d3a354970660920e4454cc08275b94a434cd6fbb
DelSkull <Deluta1989@gmail.com>
2026-03-23 11:07:38 +0300

    init: start project

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>RSS School React Course</h1>
+        <p>Проект настроен!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
